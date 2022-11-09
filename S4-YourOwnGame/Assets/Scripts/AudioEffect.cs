e753b0f56b0967770299b5b74ea82e97701936e6
Yannick <447113@student.fontys.nl>
2022-11-09 16:57:12 +0100

    Updated audio effect script

--- S4-YourOwnGame/Assets/Scripts/AudioEffect.cs
+++ S4-YourOwnGame/Assets/Scripts/AudioEffect.cs
@@ -15,10 +15,10 @@ public class AudioEffect : MonoBehaviour
     private float m_Timer = 0f;
     private float m_StepSize = 0f;
 
-    void Update()
+    void FixedUpdate()
     {
         if (m_IntroPlaying || m_OutroPlaying)
-            m_Timer += Time.deltaTime;
+            m_Timer += Time.fixedDeltaTime;
 
         if (m_IntroPlaying)
         {
@@ -42,7 +42,7 @@ public class AudioEffect : MonoBehaviour
         if (Source != null)
         {
             m_Timer = 0f;
-            m_StepSize = Interval / (MaxPitch - Source.pitch);
+            m_StepSize = (MaxPitch - Source.pitch) / ((Interval * 50) + 1);
             Source.mute = false;
             m_IntroPlaying = true;
         }
@@ -53,7 +53,7 @@ public class AudioEffect : MonoBehaviour
         if (Source != null)
         {
             m_Timer = 0f;
-            m_StepSize = Interval / (Source.pitch - MinPitch);
+            m_StepSize = (Source.pitch - MinPitch) / ((Interval * 50) + 1);
             m_OutroPlaying = true;
         }
     }
